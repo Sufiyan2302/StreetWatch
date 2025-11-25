2ce55ac8d9d98ecc090b922cfaab0b497b4d53ff
Sufiyan2302 <sufffiyan3@gmail.com>
2025-11-26 00:14:48 +0530

    Update firebase.js

--- src/firebase.js
+++ src/firebase.js
@@ -10,7 +10,7 @@ import { getAuth } from "firebase/auth";
 // Your web app's Firebase configuration
 // For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
-  apiKey: "AIzaSyBksVSGLrLCMDGBffTzNQ6ddNzGr83tNEo",
+  apiKey: "YOUR_API_KEY",
   authDomain: "create-report-c8e81.firebaseapp.com",
   projectId: "create-report-c8e81",
   storageBucket: "create-report-c8e81.firebasestorage.app",
